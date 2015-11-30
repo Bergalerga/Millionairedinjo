36724c37e8f1278bf97025049348b5f75da0ad2d
Fredrik Christoffer Berg <fredrcb@stud.ntnu.no>
2015-11-30 12:53:01 +0100

    colors n stuf

--- js/millionare.js
+++ js/millionare.js
@@ -1,14 +1,12 @@
-var Millionare  = (function($) {
+var questions = []
+var nextQuestion = 0
+var lastCorrectAnswer = -1
 
-    var questions = []
-    var nextQuestion = 0
-    var lastCorrectAnswer = -1
+var Millionare  = (function($) {
 
     var getQuestion = function(id) {
         return questions[id]
     }
-
-
 
     return {
         init: function(url) {
@@ -44,6 +42,13 @@ var Millionare  = (function($) {
 
 var showNextQuestion = true;
 
+function reset() {
+    document.getElementById('question-1').style.background = "black";
+    document.getElementById('question-2').style.background = "black";
+    document.getElementById('question-3').style.background = "black";
+    document.getElementById('question-4').style.background = "black";
+}
+
 $(document).ready(function() {
     console.log('loaded document')
     Millionare.init('questions/questions.json')
@@ -55,6 +60,41 @@ $(document).ready(function() {
                 alert('Correct is ' + Millionare.lastCorrectAnswer())
             }
             showNextQuestion = !showNextQuestion
+        }
+        else if (e.which == 65 || e.which == 97) {
+            if (lastCorrectAnswer == 1) {
+                document.getElementById('question-1').style.background = "green";
+            }
+            else {
+                document.getElementById('question-1').style.background = "red";
+            }
+        }
+        else if (e.which == 66 || e.which == 98) {
+            if (lastCorrectAnswer == 2) {
+                document.getElementById('question-2').style.background = "green";
+            }
+            else {
+                document.getElementById('question-2').style.background = "red";
+            }
+        }
+        else if (e.which == 67  || e.which == 99) {
+            if (lastCorrectAnswer == 3) {
+                document.getElementById('question-3').style.background = "green";
+            }
+            else {
+                document.getElementById('question-3').style.background = "red";
+            }
+        }
+        else if (e.which == 68 || e.which == 100) {
+            if (lastCorrectAnswer == 4) {
+                document.getElementById('question-4').style.background = "green";
+            }
+            else {
+                document.getElementById('question-4').style.background = "red";
+            }
+        }
+        else {
+            reset()
         }
     })
 })
