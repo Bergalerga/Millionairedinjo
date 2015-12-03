c8b1e3073a949e41d80c7adbb7f4ba95721afa4f
Håkon Ødegård Løvdal <me@hakloev.no>
2015-12-03 18:00:54 +0100

    cleaned up some js, adapted it to module pattern

--- js/millionare.js
+++ js/millionare.js
@@ -1,20 +1,141 @@
 var Millionare  = (function($) {
 
-    var questions = []
-    var nextQuestion = 0
-    var lastCorrectAnswer = -1
+    var questions = [];
+    var nextQuestion = 0;
+    var lastCorrectAnswer = -1;
+    // 50/50 Specific
+    var reducedAlternatives = false;
+    // Call Specific
+    var hasCalled = false;
+    var countdown;
+    var running = false;
+    var calling = false;
+
+    var sounds = {
+        /* Chrome supports .wav and loop, but Safari doesn't */
+        'duringCallSound': new Audio('sounds/call_question.wav'),
+        'beforeCallSound': new Audio('sounds/call_calling.wav'),
+        'finishedCallSound': new Audio('sounds/telefonjoker_ende.mp3'),
+        'standardSound': new Audio('sounds/stufe_3.mp3')
+    };
+
+    var bindListeners = function() {
+        $("#help-call").on("click", function () {
+            if (hasCalled) return;
+            $(this).css('opacity', '0.3');
+
+            $(document.body).append('<div id="call"></div>');
+
+            var call = $("#call");
+            countdown = call.countdown360({
+                radius      : call.width(),
+                seconds     : 30,
+                fontColor   : '#FFFFFF',
+                strokeStyle : '#FF9933',
+                fillStyle   : '#006699',
+                autostart   : false,
+                label       : false,
+                onComplete  : function () {
+                    fadeOutClock();
+                    calling = false;
+                    running = false;
+                }
+            });
+
+            sounds['beforeCallSound'].play();
+            sounds['standardSound'].pause();
+
+            calling = true;
+            fadeInClock();
+            hasCalled = true;
+        });
+
+        $('#help-50').on('click', function() {
+            if (!reducedAlternatives) {
+                removeTwoAlternatives();
+                reducedAlternatives = !reducedAlternatives;
+            }
+            $(this).css('opacity', '0.3');
+        });
+
+        $(document).keypress(function(e) {
+            if (e.keyCode == 32) {
+                if(calling) {
+                    if (!running) {
+                        sounds['beforeCallSound'].pause();
+                        sounds['duringCallSound'].play();
+                        countdown.start();
+                        running = true;
+                    } else {
+                        sounds['duringCallSound'].pause();
+                        sounds['finishedCallSound'].play();
+                        countdown.stop();
+                        fadeOutClock();
+                        calling = false;
+                        running = false;
+                    }
+                }
+            }
+        });
+    };
+
+    var fadeInClock = function () {
+        var pos = -140;
+        var clock = $('#call');
+        clock.css('right', pos);
+        var interval = setInterval(function () {
+            pos+=5;
+            clock.css('right', pos);
+            if(pos >= 210) {
+                clearInterval(interval);
+            }
+        },1)
+    };
+
+    var fadeOutClock = function () {
+        var pos = 210;
+        var clock = $('#call');
+        clock.css('right', pos);
+        var interval = setInterval(function () {
+            pos-=5;
+            clock.css('right', pos);
+            if(pos <= -140) {
+                clearInterval(interval);
+                sounds['standardSound'].play();
+                clock.remove();
+            }
+        },1)
+    };
+
+    var activateSounds = function() {
+        sounds['beforeCallSound'].loop = true;
+        sounds['standardSound'].loop = true;
+        sounds['standardSound'].autoplay = true;
+    };
 
     var getQuestion = function(id) {
         return questions[id]
-    }
+    };
 
     var resetQuestionStyle = function() {
         $('#alternatives').children().css('background-color', '#223e95');
-    }
+    };
+
+    var removeTwoAlternatives = function() {
+        var alternatives = [1, 2, 3, 4];
+        alternatives.splice(alternatives.indexOf(lastCorrectAnswer), 1);
+        alternatives.splice(alternatives[Math.floor(Math.random() * alternatives.length)], 1);
+        $("#alternative-" + alternatives[0].toString()).html("");
+        $("#alternative-" + alternatives[1].toString()).html("");
+    };
 
     return {
         init: function(url) {
-            console.log('Initiating Millionare.js')
+            console.log('Initiating Millionare.js');
+            bindListeners();
+            activateSounds();
+
+            // Load questions
             $.ajax({
                 url: url,
                 dataType: 'json',
@@ -28,41 +149,24 @@ var Millionare  = (function($) {
             });
         },
         populateNextQuestion: function() {
-            question = getQuestion(nextQuestion)
-            lastCorrectAnswer = question.correct
+            question = getQuestion(nextQuestion);
+            lastCorrectAnswer = question.correct;
             nextQuestion++
-            $('#question h1').html(question.question)
+            $('#question h1').html(question.question);
             $(question.alternatives).each(function(index, alternative) {
-                console.log(0)
-                id = '#alternative-' + (index + 1)
+                id = '#alternative-' + (index + 1);
                 $(id).html(alternative);
             });
         },
         lastCorrectAnswer: function() {
             return lastCorrectAnswer
         },
+        sounds: sounds,
         reset: resetQuestionStyle
     }
-
 })(jQuery);
 
 var showNextQuestion = true;
-
-//audio
-var duringCallSound = new Audio('sounds/call_question.wav');
-var beforeCallSound = new Audio('sounds/call_calling.wav');
-var finishedCallSound = new Audio('sounds/telefonjoker_ende.mp3');
-var standardSound = new Audio('sounds/stufe_3.mp3');
-beforeCallSound.loop = true;
-standardSound.loop = true;
-standardSound.autoplay = true;
-
-
-//call
-var countdown;
-var running = false;
-var calling = false;
-
 
 $(document).ready(function() {
     Millionare.init('questions/questions.json')
@@ -95,92 +199,8 @@ $(document).ready(function() {
             question = $('#question-4');
             (lastCorrectAnswer == 4) ? $(question).css('background-color', 'green') : $(question).css('background-color', 'red')
         }
-        //Keyboard button 5
-        else if (e.keyCode == 53) {
-            var answs = [1,2,3,4]
-            console.log(answs.indexOf(lastCorrectAnswer))
-            answs.splice(answs.indexOf(lastCorrectAnswer), 1)
-            answs.splice(answs[Math.floor(Math.random()*answs.length)], 1)
-            q1 = $("#alternative-" + answs[0].toString()).html("");
-            q2 = $("#alternative-" + answs[1].toString()).html("");
-        }
-        //spacebar
-        else if (e.keyCode == 32) {
-            if(calling) {
-                if (!running) {
-                    beforeCallSound.pause();
-                    duringCallSound.play();
-                    countdown.start();
-                    running = true;
-                } else {
-                    duringCallSound.pause();
-                    finishedCallSound.play();
-                    countdown.stop();
-                    fadeOutClock();
-                    calling = false;
-                    running = false;
-                }
-            }
-        }
         else {
             Millionare.reset();
         }
     });
-
-
-    $("#help-call").on("click", function () {
-
-        $(document.body).append('<div id="call"></div>');
-
-        var call = $("#call");
-
-        countdown = call.countdown360({
-            radius      : call.width(),
-            seconds     : 30,
-            fontColor   : '#FFFFFF',
-            strokeStyle : '#FF9933',
-            fillStyle   : '#006699',
-            autostart   : false,
-            label       : false,
-            onComplete  : function () {
-                fadeOutClock();
-                calling = false;
-                running = false;
-            }
-        });
-
-        duringCallSound = new Audio('sounds/call_question.wav');
-        beforeCallSound.play();
-        standardSound.pause();
-        calling = true;
-
-        fadeInClock();
-    });
 });
-
-var fadeInClock = function () {
-    var pos = -140;
-    var clock = $('#call');
-    clock.css('right', pos);
-    var interval = setInterval(function () {
-        pos+=5;
-        clock.css('right', pos);
-        if(pos >= 210) {
-            clearInterval(interval);
-        }
-    },1)
-};
-var fadeOutClock = function () {
-    var pos = 210;
-    var clock = $('#call');
-    clock.css('right', pos);
-    var interval = setInterval(function () {
-        pos-=5;
-        clock.css('right', pos);
-        if(pos <= -140) {
-            clearInterval(interval);
-            standardSound.play();
-            clock.remove();
-        }
-    },1)
-};
